38b84fb885989cdae136aa510cab944afef921bd
wartyfish <jamiefendt96@gmail.com>
2025-12-31 13:07:33 +0000

    Updated functionality of Zip folder snapshots

--- 2025/Automate the Boring Stuff/Chapter 11 Organising Files/ZIP_Folder_Snapshots/ZIP_folder_snapshots.py
+++ 2025/Automate the Boring Stuff/Chapter 11 Organising Files/ZIP_Folder_Snapshots/ZIP_folder_snapshots.py
@@ -3,20 +3,31 @@ ZIP snapshot generator.
 
 Program backs-up selected folder to a new ZIP folder each time it's run.
 """
-
 import os, zipfile
 from pathlib import Path
 
-# get new snapshot path
-i=0
-while True:
-    zip_name = f"snapshot_{i:03}.zip"
-    p = Path("snapshots") / zip_name
-    if p.is_file():
-        i+=1
-    else:
-        break
+def compress_to_zip(folder_name, target_dir):
+    folder_path = Path(folder_name)
+    target_path = Path(target_dir)
 
-# create new snapshot to p
-with zipfile.ZipFile(p, "w") as z:
-    z.write("Test_folder", compress_type=zipfile.ZIP_DEFLATED)
+    # create target directory if it doesn't yet exist
+    if not target_path.is_dir():
+        os.makedirs(target_path)
+
+    # get new snapshot path
+    i=0
+    while True:
+        zip_name = f"snapshot_{i:03}.zip"
+        p = target_path / zip_name
+        if p.is_file():
+            i+=1
+        else:
+            break
+
+    # create new snapshot to p
+    with zipfile.ZipFile(p, "w", zipfile.ZIP_DEFLATED) as zipobj:
+        for file_path in folder_path.rglob("*"):
+            if file_path.is_file():
+                zipobj.write(file_path, arcname=file_path.relative_to(folder_path))
+
+compress_to_zip("Test_folder", "snapshots")
