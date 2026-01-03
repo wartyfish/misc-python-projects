1aeef9eed18a19070a3677bc8bf776f63bd1b72c
Jamie <jamiefendt96@gmail.com>
2026-01-03 23:51:23 +0000

    Brute-force solution to fill gaps question

--- 2025/Automate the Boring Stuff/Chapter 11 Organising Files/Close gaps/close_gaps.py
+++ 2025/Automate the Boring Stuff/Chapter 11 Organising Files/Close gaps/close_gaps.py
@@ -1,12 +1,12 @@
-import os
+import os, shutil
 from pathlib import Path
 
 def make_gaps(target):
     p = Path(target)
     if not p.is_dir():
         p.mkdir()
-    for i in range(200):
-        if i not in (3, 54, 69, 102, 103, 197):
+    for i in range(11):
+        if i not in (3, 5, 7):
             f = p / f"spam{i:03d}.txt"
             with open(f, "w") as file:
                 pass
@@ -21,26 +21,41 @@ def fill_gaps(target):
     def find_next_file(file):
         next_file = p / file_plus_one(file)
         if os.path.isfile(next_file):
-            print(f"Next file: {next_file}")
             return next_file
         else:
-            find_next_file(next_file)
+            return find_next_file(next_file)
 
-    for f in p.glob("*"):
-        if f == p / "spam199.txt":
+    number_of_files = len([f for f in p.iterdir()])
+    print(number_of_files)
+    i = 0
+    while True:
+        if i == number_of_files:
             break
-        f_plus_one = p / file_plus_one(f)
-        if not os.path.isfile(f_plus_one):
-            print(f"{file_plus_one(f)} missing")
-            print(f"Next file: {find_next_file(f)}")
-        
-        
+        for f in p.iterdir():
+            i += 1
+            if i == number_of_files:
+                break
+            f_plus_one = p / file_plus_one(f)
+            if not os.path.isfile(f_plus_one):
+                next_found_file = find_next_file(f)
+                os.rename(next_found_file, f_plus_one)
+                i = 0
+                break
 
+def delete_files(target):
+    p = Path(target)
+
+    for f in p.iterdir():
+        if f.is_file() or f.is_symlink():
+            f.unlink()
+        elif f.is_dir():
+            shutil.rmtree(f)
 
 def main():
     p = r"misc-python-scripts\2025\Automate the Boring Stuff\Chapter 11 Organising Files\Close gaps\Files"
-    if True:
-        make_gaps(p)
+    
+    delete_files(p)
+    make_gaps(p)
     fill_gaps(p)
     
 
