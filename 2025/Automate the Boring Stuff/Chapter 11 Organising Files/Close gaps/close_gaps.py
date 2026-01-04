b796ceef38f1d886658db5dd06e93f097e5a4b17
Jamie <jamiefendt96@gmail.com>
2026-01-04 19:42:02 +0000

    Added make_gaps function to renumber chronologically named files to include specified gaps in file numbers

--- 2025/Automate the Boring Stuff/Chapter 11 Organising Files/Close gaps/close_gaps.py
+++ 2025/Automate the Boring Stuff/Chapter 11 Organising Files/Close gaps/close_gaps.py
@@ -1,14 +1,14 @@
 """
 Close gaps
 
-make_gaps function populates "Files" directory with numerically names "spamXXX.txt" files, with specified gaps in file numbers.
+make_files function populates "Files" directory with numerically names "spamXXX.txt" files, with specified gaps in file numbers.
  
 """
 
 import os, shutil, re
 from pathlib import Path
 
-def make_gaps(target):
+def make_files(target):
     p = Path(target)
     if not p.is_dir():
         p.mkdir()
@@ -35,9 +35,37 @@ def fill_gaps(target):
     # identify files that need to be renamed and then do so
     for i in indexed_files:
         if i != re.match(pattern, indexed_files[i].name).group(1):
-            new_name = re.sub(r"\d{3}", i, indexed_files[i].name)
-            print(f"{indexed_files[i].name} renamed to {new_name}")
+            new_name = re.sub(r"\d{3}", i, indexed_files[i].name)            
             os.rename(indexed_files[i], p / new_name)
+            print(f"{indexed_files[i].name} renamed to {new_name}")
+
+def make_gaps(target, gaps=list):
+    p = Path(target)
+    pattern = re.compile(r"spam(\d{3}).txt")
+
+    # as before, populate dict with indexed filenumbers
+    # this time with specified gaps
+    indexed_files = {}
+    i = 0
+    for f in p.iterdir():
+        # generate file indices with gaps
+        while i in gaps:
+            i += 1
+        
+        file_index = f"{i:03d}"
+        valid = re.match(pattern, f.name)
+        if valid:
+            indexed_files[file_index] = f
+        i += 1
+
+    # identify files to be renamed, this time working in reverse order
+    for i in reversed(indexed_files):
+        if i != re.match(pattern, indexed_files[i].name).group(1):
+            new_name = re.sub(r"\d{3}", i, indexed_files[i].name)          
+            os.rename(indexed_files[i], p / new_name)
+            print(f"{indexed_files[i].name} renamed to {new_name}")
+        
+
 
 def delete_files(target):
     p = Path(target)
@@ -52,8 +80,10 @@ def main():
     p = r"misc-python-scripts\2025\Automate the Boring Stuff\Chapter 11 Organising Files\Close gaps\Files"
     
     delete_files(p)
-    make_gaps(p)
+    make_files(p)
     fill_gaps(p)
+    gaps = [2, 4, 6, 8]
+    make_gaps(p, gaps)
     
 
 if __name__ == "__main__":
