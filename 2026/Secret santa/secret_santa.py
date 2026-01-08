93926e23f5e3ba035bd47ea79cc447bc8678388a
Jamie <jamiefendt96@gmail.com>
2026-01-08 13:01:59 +0000

    Started implementing derangement logic

--- 2026/Secret santa/secret_santa.py
+++ 2026/Secret santa/secret_santa.py
@@ -1,25 +1,15 @@
 import random
 
 def pull_from_hat(participants: list, excluded_pairs: dict = None) -> dict:
-    if excluded_pairs is None:
-        excluded_pairs = {}
-    
-    for name in participants:
-        excluded_pairs.setdefault(name, [])
-
-    gifters = [name for name in participants]
-    receivers = [name for name in participants]
-
-    paired = {}
-    for name in gifters:
-        other_names = [n for n in receivers if n != name and n not in excluded_pairs[name]]
-        chosen = other_names[random.randrange(len(other_names))]
-        paired[name] = chosen
-        receivers.remove(chosen)
-
-        print(f"{name:10} is gifting {chosen}")
+    derangement_factor = random.randrange(1, len(participants))
+    participants_deranged = []
+    for i in range(len(participants)):
+        j = (i + derangement_factor) % len(participants)
+        print(participants[j])
+    print(derangement_factor)
+    print(participants_deranged)
         
-def define_groups(my_list):
+def define_groups(my_list) -> list:
     random.shuffle(my_list)
     groups = []
     groups_index = 0
@@ -27,21 +17,22 @@ def define_groups(my_list):
     groups.append([my_list[0]])
     
     for item in my_list:
-        item_count += 1
-        # make sure group size is at least 2
-        if len(groups[groups_index]) == 1 or len(my_list) - item_count <= 1:
-            groups[groups_index].append(item)
-        # if allowed, each successive item has 50% change of joining group
-        else:
-            if random.randrange(2) == 0:
+        if item != my_list[0]:
+            item_count += 1
+            # make sure group size is at least 2
+            if len(groups[groups_index]) == 1 or len(my_list) - item_count <= 1:
                 groups[groups_index].append(item)
+            # if allowed, each successive item has 50% change of joining group
             else:
-                groups_index += 1
-                groups.append([item])
+                if random.randrange(2) == 0:
+                    groups[groups_index].append(item)
+                else:
+                    groups_index += 1
+                    groups.append([item])
     
     return groups
 
-def group_distributions(list, iterations):
+def group_size_frequencies(list, iterations) -> dict:
     number_of_groups_count = {}
     [number_of_groups_count.setdefault(n, 0) for n in range(len(list) // 2 + 1)]
 
@@ -50,18 +41,30 @@ def group_distributions(list, iterations):
 
     return number_of_groups_count
 
+def print_distribution(my_list, iterations) -> None:
+    distribution = group_size_frequencies(my_list, iterations)
+    print(distribution)
+    for n in distribution:
+        print(f"Group size {n:2}: {"x"*int(round((int(distribution[n])/100)))}")
 
 
-participants = ["Alice", "Bob", "Charlie", "David", "Emma", "Frank", "Gabriel", "Hannah", "Imogen", "Jamie"]
+
+
+participants = ["Alice", "Bob", "Charlie", "David", "Emma", "Frank", "Gabriel", "Hannah", "Imogen", "Jamie", "Kate",
+                "Liam", "Max", "Nick", "Olivia", "Peter", "Quentin", "Racheal", "Simon", "Tina", "Uma", "Velma", "Wilson",
+                "Xanvier", "Yorik", "Zack"]
 
 disallowed = {}
 disallowed["Alice"] = ["Bob", "David"]
 disallowed["Bob"]   = ["Alice", "David"]
 disallowed["David"] = ["Alice", "Bob"]
 
+group_size = 3
+participants = participants[:group_size]
+
+pull_from_hat(participants)
+
 iterations = 10000
+distribution = group_size_frequencies(participants, iterations)
 
-distribution = group_distributions(participants, iterations)
 
-for n in distribution:
-    
